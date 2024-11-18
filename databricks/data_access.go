fb47ca6efcc270514eadc95c1c24a86e19cfaee6
Ruben Mennes <ruben.mennes@me.com>
2024-11-18 14:59:33 +0100

    Fix: check if binding is not null

--- databricks/data_access.go
+++ databricks/data_access.go
@@ -1645,7 +1645,7 @@ func (t *MetastoreRepoCache) GetCatalogRepo(ctx context.Context, metastoreId str
 				continue
 			}
 
-			if binding.BindingType == catalog.WorkspaceBindingBindingTypeBindingTypeReadOnly {
+			if binding != nil && binding.BindingType == catalog.WorkspaceBindingBindingTypeBindingTypeReadOnly {
 				possibleUnpreferredReadRepos = append(possibleUnpreferredReadRepos, possibleRepo)
 			} else {
 				possibleUnpreferredWriteRepos = append(possibleUnpreferredWriteRepos, possibleRepo)
